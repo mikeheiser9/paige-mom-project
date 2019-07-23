83c767c568147483d2a1e7d6dcae0e2714cae6a8
mikeheiser9 <mikeheiser9@gmail.com>
2019-07-22 21:36:03 -0400

    loading

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var express = require("express");
 var bodyParser = require("body-parser");
 // Tells node that we are creating an "express" server
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 5000;
 
-var db = require("./app/models")
+// var db = require("./app/models");
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,18 +21,18 @@ app.use(express.static("./app/public"));
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./app/routes/api-routes")(app);
-require("./app/routes/htmlRoutes")(app);
+// require("./app/routes/api-routes")(app);
+require("./app/routes/html-routes")(app);
 
 // =============================================================================
 // LISTENER
 // The below code effectively "starts" our server
 // =============================================================================
 
-db.sequelize.sync({}).then(function () {
-  console.log("db synced...");
+// db.sequelize.sync({}).then(function () {
+//   console.log("db synced...");
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
-});
+// });
