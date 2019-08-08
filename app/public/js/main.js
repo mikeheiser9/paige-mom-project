43621dc62dafa9ce032ea4c29a71fbc448705822
mikeheiser9 <mikeheiser9@gmail.com>
2019-08-08 01:05:44 -0400

    js and  markup done

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -270,5 +270,51 @@ $(document).ready(function($) {
     fixedContentPos: false
   });
 
+
+  $("#jscriptContainer").html('<div class="flex-img-row"><div class="flex-img-column"><img src="./images/horse-stock.jpg" style="width:100%"><img src="./images/horse1.JPG" style="width:100%"><img src="./images/horse-sunset.JPG" style="width:100%"><img src="./images/horse4.JPG" style="width:100%"><img src="./images/tape-on-horse.JPG" style="width:100%"><img src="./images/stables.JPG" style="width:100%"></div><div class="flex-img-column"><img src="./images/horse3.JPG" style="width:100%"><img src="./images/tape-on-horse-3.JPG" style="width:100%"><img src="./images/horse-care.jpg" style="width:100%"><img src="./images/ankle-tape-2.JPG" style="width:100%"><img src="./images/horse-sketch.JPG" style="width:100%"><img src="./images/horse5.JPG" style="width:100%"><img src="./images/horse2.JPG" style="width:100%"></div></div><div class="learn-more-link"><a href="javascript:void(0)"><p>Learn More</p></a></div>');
+
+  var homeRoute = function () {
+	$("#home").on('click', function(ev) {
+		ev.preventDefault();
+		loader();
+		$("#jscriptContainer").empty();
+		$("#jscriptContainer").html('<div class="flex-img-row"><div class="flex-img-column"><img src="./images/horse-stock.jpg" style="width:100%"><img src="./images/horse1.JPG" style="width:100%"><img src="./images/horse-sunset.JPG" style="width:100%"><img src="./images/horse4.JPG" style="width:100%"><img src="./images/tape-on-horse.JPG" style="width:100%"><img src="./images/stables.JPG" style="width:100%"></div><div class="flex-img-column"><img src="./images/horse3.JPG" style="width:100%"><img src="./images/tape-on-horse-3.JPG" style="width:100%"><img src="./images/horse-care.jpg" style="width:100%"><img src="./images/ankle-tape-2.JPG" style="width:100%"><img src="./images/horse-sketch.JPG" style="width:100%"><img src="./images/horse5.JPG" style="width:100%"><img src="./images/horse2.JPG" style="width:100%"></div></div><div class="learn-more-link"><a href="javascript:void(0)"><p>Learn More</p></a></div>');
+		$('body').removeClass('menu-show');
+				setTimeout(function(){
+					$('#colorlib-main-nav > .js-colorlib-nav-toggle').addClass('show');
+				}, 100);
+	})
+}
+homeRoute();
+
+
+
+var aboutZoeRoute = function () {
+	$("#about").on('click', function(ev) {
+		ev.preventDefault();
+		$("#jscriptContainer").empty();
+		$("#jscriptContainer").html('<div class="row"><div class="flex-container-col"><div class="header-row-left"><h2 class="section-header">About Zoe</h2></div></div></div><div class="row"><div class="bio-row"><div class="text-body-img"><img src="./images/horse5.JPG" class="text-img"><p class="bio-txt">Dear horse lovers,<br><br>I rode my first horse at 5 years old only weeks before a ten day camping trip on horseback with my sister and parents, each of us on our own horses.  There was a mix of utter terror and love for these beautiful creatures.<br><br>My sister and I were only ever able to save enough birthday and Christmas money to buy horses no one else wanted, so the physically and emotionally challenged horse is no stranger to me.  My own horse requires special understanding and regular Equi-Bow and Equi-Tape to help support him from a past that has left him with some physical and emotional scars.<br><br>It is this love and desire that led me to a career in support of equine health.  It is a great honour to be asked to help a horse, mini or donkey.  I love what I do and I am so grateful to all the equines that have taught me so graciously.<br><br>In keeping with raising our awareness to our horses’ individual needs to attain optimal health,I feel that we can support our horses more if we can keep our own bodies balanced and fit so that our horses are not needing to adjust their own bodies to compensate for our imbalances and areas we could be stronger.  In support of our own health and in turn, that of our beloved horse, I am able to design individual no nonsense programs for personal goals to do alone and/or also programs you can do with your horse out of saddle that are very effective, very fun and wonderful opportunities for other means of bonding with your horse<br><br>Namaste.<br><br>Zoё Mercer</p></div></div></div>');
+		$('body').removeClass('menu-show');
+				setTimeout(function(){
+					$('#colorlib-main-nav > .js-colorlib-nav-toggle').addClass('show');
+				}, 100);
+	})
+}
+aboutZoeRoute();
+
+var aboutTapeRoute = function () {
+	$("#learn").on('click', function(ev) {
+		ev.preventDefault();
+		$("#jscriptContainer").empty();
+		$("#jscriptContainer").html('<div class="row"><div class="flex-container-col"><div class="header-row-left"><h2 class="section-header">About Equine Support</h2></div></div></div><div class="row"><div class="bio-row"><div class="text-body-img"><img src="./images/tape-on-horse.JPG" class="text-img"><p class="section-header">What is Equi-Bow?</p><p class="bio-txt"><br><br>I rode my first horse at 5 years old only weeks before a ten day camping trip on horseback with my sister and parents, each of us on our own horses.  There was a mix of utter terror and love for these beautiful creatures.</p><p class="section-header">Equi-Bow in Greater Depth</p><p class="bio-txt"><br><br>I rode my first horse at 5 years old only weeks before a ten day camping trip on horseback with my sister and parents, each of us on our own horses.  There was a mix of utter terror and love for these beautiful creatures.</p><p class="bio-txt">I rode my first horse at 5 years old only weeks before a ten day camping trip on horseback with my sister and parents, each of us on our own horses.  There was a mix of utter terror and love for these beautiful creatures.</p><p class="section-header">Is Equi-Bow going to be something my horse will need all its life?</p><p class="bio-txt"><br><br>I rode my first horse at 5 years old only weeks before a ten day camping trip on horseback with my sister and parents, each of us on our own horses.  There was a mix of utter terror and love for these beautiful creatures.</p><p class="bio-txt">I rode my first horse at 5 years old only weeks before a ten day camping trip on horseback with my sister and parents, each of us on our own horses.  There was a mix of utter terror and love for these beautiful creatures.</p></div></div></div>');
+
+		$('body').removeClass('menu-show');
+				setTimeout(function(){
+					$('#colorlib-main-nav > .js-colorlib-nav-toggle').addClass('show');
+				}, 100);
+	})
+}
+aboutTapeRoute();
+
 });
 
